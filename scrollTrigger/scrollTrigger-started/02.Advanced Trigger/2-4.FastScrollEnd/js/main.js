a5511642f6296949744eed17708d531e41a92012
ssooya <ssooya90@naver.com>
2024-02-25 23:22:04 +0900

    feat PreventSCroll

--- scrollTrigger/scrollTrigger-started/02.Advanced Trigger/2-4.FastScrollEnd/js/main.js
+++ scrollTrigger/scrollTrigger-started/02.Advanced Trigger/2-4.FastScrollEnd/js/main.js
@@ -2,9 +2,21 @@
 
 
 
+const gotoTop = gsap.timeline()
+		.to('.goToTop img',{y:0, opacity:1, ease:'back(3)'})
+		.to('.gotoTop a',{y:0, opacity: 1, ease : 'back(3)'},'-=0.3')
 
 
-
+ScrollTrigger.create({
+	trigger : '.scroll-content',
+	start : '75% center',
+	end : 'bottom center',
+	animation : gotoTop,
+	pin : false,
+	markers : true,
+	//                 E    L   EB    LB
+	toggleActions : 'play none none reverse'
+})
 
 
 
