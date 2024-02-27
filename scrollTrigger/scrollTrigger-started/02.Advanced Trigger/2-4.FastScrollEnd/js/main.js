e00891e7be9e3d063b8288537759e86c1c7620bd
ssooya <ssooya90@naver.com>
2024-02-27 21:13:28 +0900

    feat PreventSCroll

--- scrollTrigger/scrollTrigger-started/02.Advanced Trigger/2-4.FastScrollEnd/js/main.js
+++ scrollTrigger/scrollTrigger-started/02.Advanced Trigger/2-4.FastScrollEnd/js/main.js
@@ -2,24 +2,56 @@
 
 
 
-const gotoTop = gsap.timeline()
-		.to('.goToTop img',{y:0, opacity:1, ease:'back(3)'})
-		.to('.gotoTop a',{y:0, opacity: 1, ease : 'back(3)'},'-=0.3')
+// const gotoTop = gsap.timeline()
+// 		.to('.goToTop img',{y:0, opacity:1, ease:'back(3)'})
+// 		.to('.gotoTop a',{y:0, opacity: 1, ease : 'back(3)'},'-=0.3')
+//
+//
+// ScrollTrigger.create({
+// 	trigger : '.scroll-content',
+// 	start : '75% center',
+// 	end : 'bottom center',
+// 	animation : gotoTop,
+// 	pin : false,
+// 	markers : true,
+// 	//                 E    L   EB    LB
+// 	toggleActions : 'play none none reverse'
+// })
+//
 
+const topButton = document.querySelector('.goToTop');
 
-ScrollTrigger.create({
-	trigger : '.scroll-content',
-	start : '75% center',
-	end : 'bottom center',
-	animation : gotoTop,
-	pin : false,
-	markers : true,
-	//                 E    L   EB    LB
-	toggleActions : 'play none none reverse'
+topButton.addEventListener('click',() => {
+
+	scrollbar.scrollTo(0,0,600,{
+		callback : () => {
+			console.log("done")
+		}
+	})
+
 })
 
+const gotoTop = gsap.timeline()
+		.to('.goToTop img',{
+			y : 0,
+			opacity : 1,
+			ease : 'back(3)'
+		})
+		.to('.goToTop a',{
+			y : 0,
+			opacity : 1,
+			ease : 'back(3)'
+		},'-=0.3')
 
-
+ScrollTrigger.create({
+	trigger : '.section04',
+	start : '-10% top',
+	end : '30% top',
+	animation : gotoTop,
+	markers : true,
+	//               E    L    EB   LB
+	toggleActions : 'play none none reverse'
+})
 
 
 
