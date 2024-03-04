3d10b9cfd2e01b2acb5d99c025209841030ead99
ssooya <ssooya90@naver.com>
2024-03-04 23:45:51 +0900

    feat advanced part

--- scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-2.Text Effects/js/main.js
+++ scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-2.Text Effects/js/main.js
@@ -1,13 +1,79 @@
 
 
+let split
+
+function init() {
+
+	split = new SplitText('p',{type : 'lines'})
+	const splitCover = new SplitText('p',{type : 'lines' , linesClass:'cover'})
+
+	split.lines.forEach((line, index)=>{
+
+		ScrollTrigger.create({
+			trigger : splitCover.lines[index],
+			start : 'top 90%',
+			end : 'bottom center',
+			animation : gsap.from(line,
+					{
+						opacity:0,
+						filter:'blur(10px)',
+						y:300,
+						transformOrigin: '50% 50% -50%',
+						rotateX :-180
+					}
+			),
+			markers : true,
+			scrub : true,
+
+		})
+	})
+
+	markers();
+
+}
+
+
+const debounce = (callback, time = 500) => {
+
+	let timeOut;
+
+	// 클로저 형태
+	return function (...args){
+		clearTimeout(timeOut)
+		timeOut = setTimeout( () => {
+			callback.apply(this, args);
+		} , time)
+	}
 
 
 
 
 
+}
+
+// 이벤트가 끝나는 시점에 한번만 실행되도록
+debounce( () => {
+	console.log('hi')
+},2000)
+
+// const id = setTimeout(() => {
+// 	console.log("hi")
+// }, 5000)
+//
+// clearTimeout(3);
 
 
+function killAll(){
+	split.revert();
+	ScrollTrigger.getAll().forEach((item) => {
+		item.kill
+	});
+	init();
+}
 
+
+window.addEventListener('load',init);
+window.addEventListener('resize',debounce(killAll))
 
 
 
--- scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-3.Scroll Draw SVG/js/main.js
+++ scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-3.Scroll Draw SVG/js/main.js
@@ -1,5 +1,57 @@
 
 
-// GSDevTools.create()
+
+gsap.defaults({
+	ease : 'none',
+})
+
+const pulse = gsap.timeline({
+	defaults : {
+		scale:2,
+		autoAlpha: 1,
+		transformOrigin : 'center',
+		ease : 'elastic(2.5,1'
+	}
+})
+		.to('.ball02,.text01',{},0.77)
+		.to('.ball03,.text02',{},1.2)
+		.to('.ball04,.text03',{},1.81)
+
+const line = gsap.timeline({})
+		.from('.line01',{drawSVG:0},0.84)
+		.from('.line02',{drawSVG:0},1.2)
+		.from('.line03',{drawSVG:0})
+		.from('.line04',{drawSVG:0})
+
+
+const master = gsap.timeline()
+		.to('.ball01',{autoAlpha:1, duration:0.05})
+		.from('.path',{drawSVG:0, duration: 4},0)
+		.to('.ball01',{
+			motionPath: {
+				path : '.path',
+				align : '.path',
+				alignOrigin : [0.5, 0,5]
+			},
+			duration :4
+		},0)
+		.add(pulse,0)
+		.add(line,0);
+
+
+
+
+
+ScrollTrigger.create({
+	trigger : '#svg',
+	start : 'top center',
+	end : 'bottom center',
+	animation : master,
+	scrub : true,
+	markers : true,
+})
+
+
+GSDevTools.create()
 markers()
 
