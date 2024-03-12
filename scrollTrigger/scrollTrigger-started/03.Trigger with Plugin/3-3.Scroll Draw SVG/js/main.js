84b19cdf92235c66ff10c4b9ee93dccbbbc54696
ssooya <ssooya90@naver.com>
2024-03-13 00:06:47 +0900

    feat advanced part

--- scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-3.Scroll Draw SVG/js/main.js
+++ scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-3.Scroll Draw SVG/js/main.js
@@ -1,5 +1,67 @@
 
 
+function drawSVG(element){
+
+	let pathLength;
+
+	if(typeof element !== 'string'){
+		throw new TypeError('drawSVG 함수에 전달된 인수는 string 타입 이어야 합니다.');
+	}
+	if(element.includes(',')){
+
+
+		const arr = element.split(',').map((target)=>{
+			pathLength = document.querySelector(target).getTotalLength();
+			gsap.set(target,{
+				strokeDashoffset:pathLength,
+				strokeDasharray:pathLength
+			})
+			return pathLength
+		})
+
+		console.log(arr)
+		return arr;
+	}
+
+
+	pathLength = document.querySelector(element).getTotalLength();
+
+	gsap.set(element,{
+		strokeDashoffset:pathLength,
+		strokeDasharray:pathLength
+	})
+
+	console.log(pathLength)
+
+	return pathLength;
+
+}
+
+
+drawSVG('.path,.line01,.line02');
+
+
+// const line = gsap.timeline({
+// 	defaults : {
+// 		strokeDashoffset : 0,
+// 	}
+// })
+// .to('.line01',{},0.84)
+// .to('.line02',{},1.2)
+// .to('.line03',{},)
+// .to('.line04',{},)
+
+const line = gsap.timeline({
+	defaults:{
+		strokeDashoffset:0
+	}
+})
+		.to('.line01',{},0.84)
+		.to('.line02',{},1.2)
+		.to('.line03',{},1.92)
+		.to('.line04',{},2.46)
+		.to('.line05',{},3.08)
+
 
 gsap.defaults({
 	ease : 'none',
@@ -17,28 +79,33 @@ const pulse = gsap.timeline({
 		.to('.ball03,.text02',{},1.2)
 		.to('.ball04,.text03',{},1.81)
 
-const line = gsap.timeline({})
-		.from('.line01',{drawSVG:0},0.84)
-		.from('.line02',{drawSVG:0},1.2)
-		.from('.line03',{drawSVG:0})
-		.from('.line04',{drawSVG:0})
+// const line = gsap.timeline({})
+// 		.from('.line01',{drawSVG:0},0.84)
+// 		.from('.line02',{drawSVG:0},1.2)
+// 		.from('.line03',{drawSVG:0})
+// 		.from('.line04',{drawSVG:0})
+
+
 
 
 const master = gsap.timeline()
-		.to('.ball01',{autoAlpha:1, duration:0.05})
-		.from('.path',{drawSVG:0, duration: 4},0)
+		.to('.ball01',{autoAlpha:1,duration:0.05})
+		.to('.path',{strokeDashoffset:0,duration:4},0)
 		.to('.ball01',{
-			motionPath: {
-				path : '.path',
-				align : '.path',
-				alignOrigin : [0.5, 0,5]
+			motionPath:{
+				path:'.path',
+				align:'.path',
+				alignOrigin: [0.5, 0.5]
 			},
-			duration :4
+			duration:4
 		},0)
 		.add(pulse,0)
-		.add(line,0);
+		.add(line,0)
 
 
+master.eventCallback('onUpdate',function(){
+	console.log( this._time );
+})
 
 
 
--- scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-4.SPA/js/main.js
+++ scrollTrigger/scrollTrigger-started/03.Trigger with Plugin/3-4.SPA/js/main.js
@@ -5,9 +5,96 @@
 
 
 
+function pageLeave(target){
+
+	const tl = gsap.timeline()
+			.to('h2',{y:30})
+			.to(target,{opacity:0},0)
+
+	return tl;
+
+}
+
+function pageEnter(target){
+
+	const tl = gsap.timeline()
+			.from('h2',{y:30})
+			.from(target,{opacity:0},0)
+}
+
+function home () {
+	ScrollTrigger.create({
+		trigger : ' .section03',
+		start : 'top center',
+		end : 'bottom center',
+		animation : gsap.to('.box',{x:300}),
+		markers : true,
+		scrub : true
+	})
+}
+
+function about () {
+	ScrollTrigger.create({
+		trigger : ' .section03',
+		start : 'top center',
+		end : 'bottom center',
+		animation : gsap.to('.box',{y:300}),
+		markers : true,
+		scrub : true
+	})
+}
+
+function contact () {
+	ScrollTrigger.create({
+		trigger : ' .section03',
+		start : 'top center',
+		end : 'bottom center',
+		animation : gsap.to('.box',{x:200, rotation:180, y:100}),
+		markers : true,
+		scrub : true
+	})
+}
+
+
+barba.hooks.leave(() => {
+	ScrollTrigger.getAll().forEach(t => t.kill());
+})
+
+// 모든 DOM 호출 이후
+barba.hooks.after(() => {
+
+	scrollbar.update();
+	scrollbar.scrollTo(0,0);
+
+	console.log("AFTER")
+	markers();
+
+})
 
 
 
+barba.init({
+
+	views: [
+			{namespace : "home", beforeEnter: () => home()},
+			{namespace : "about", beforeEnter: () => about()},
+			{namespace : "contact", beforeEnter: () => contact()}
+	],
+
+	transitions : [
+		{
+			name:'opacity-transition',
+			leave : (data) => pageLeave(data.current.container),
+			enter (data) {
+				pageEnter(data.next.container)
+			},
+			once : () => {
+				console.log("로드 됐을 때 딱 한번")
+				markers();
+			}
+		}
+	]
+})
 
 
 
