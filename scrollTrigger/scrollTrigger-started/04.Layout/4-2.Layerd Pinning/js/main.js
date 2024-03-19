0169c047500f04d97e2820ab642b2cf43e1d3d09
ssooya <ssooya90@naver.com>
2024-03-19 23:59:41 +0900

    feat pin layout

--- scrollTrigger/scrollTrigger-started/04.Layout/4-2.Layerd Pinning/js/main.js
+++ scrollTrigger/scrollTrigger-started/04.Layout/4-2.Layerd Pinning/js/main.js
@@ -2,6 +2,42 @@
 
 
 
+// const tl = gsap.timeline();
+
+
+gsap.utils.toArray('.section').forEach((section , index) => {
+
+
+
+
+	ScrollTrigger.create({
+		trigger:section,
+		start : 'top top',
+		// end : '+=5000',
+		pin : true,
+		pinSpacing : false,
+		// animation : tl,
+		scrub : true,
+		snap : {
+
+			snapTo : 1,
+			duration : 1,
+			ease : 'power2.inOut'
+
+		},
+		markers : true,
+	})
+
+})
+//
+// ScrollTrigger.create({
+// 	trigger:'.wrapper',
+// 	start : 'top top',
+// 	end : '+=5000',
+// 	animation : tl,
+// 	scrub : true,
+// })
+
 
 
 
