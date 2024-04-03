df13ed938c5f23c9531c8acdf1ed62eed13a1709
ssooya <ssooya90@naver.com>
2024-04-04 00:26:59 +0900

    feat mix

--- scrollTrigger/scrollTrigger-started/05.Responsive & a11y/5-1.Responsive/js/main.js
+++ scrollTrigger/scrollTrigger-started/05.Responsive & a11y/5-1.Responsive/js/main.js
@@ -12,4 +12,107 @@
 
 
 
+const box = document.querySelector('.box');
 
+
+// gsap.to(box,{
+// 	x:'100vw',
+// 	xPercent:-100,
+// 	yoyo : true,
+// 	repeat : -1, // 무한반복
+// 	duration : 2,
+// 	ease : 'power3.inOut',
+// })
+
+
+/*
+mm.add("(max-width:500px)", (context) => {
+
+	// context.spin2 = function Tiger () {
+	// 	return 'tiger';
+	// }
+
+	context.add('spin',() => {
+		gsap.to(box,{
+			rotation: 360,
+			duration : 2,
+			repeat : -1,
+			ease : 'none'
+		})
+	})
+
+	box.addEventListener('click',context.spin)
+
+
+	// 클린업
+	return () => {
+		box.removeEventListener('click',context.spin)
+		console.log('clean up')
+
+	}
+
+
+})
+*/
+
+
+// mm.add("(min-width:501px)", () => {
+// 	gsap.to(box,{
+// 		rotation: -360,
+// 		duration : 2,
+// 	})
+// })
+
+
+const options = {
+	isMobile : '(max-width:500px)',
+	isDesktop : '(min-width:501px)'
+}
+
+//
+// mm.add(options,(ctx)=>{
+// 	console.log(ctx)
+//
+// 	const {isMobile,isDesktop} = ctx.conditions;
+//
+// 	gsap.to(box,{
+// 		rotation: () => {
+//
+// 			if(isMobile){
+// 				return 360
+// 			}else{
+// 				return -360
+// 			}
+// 		},
+// 		duration : 2,
+// 		ease : 'none',
+// 	})
+//
+//
+// })
+
+const wrapper = document.querySelector('.wrapper');
+const mm = gsap.matchMedia(wrapper); // 매개변수를 넣으면 디폴트 스포크, 안넣으면 스포크 없음
+
+
+mm.add(options,(ctx)=>{
+	console.log(ctx)
+
+	const {isMobile,isDesktop} = ctx.conditions;
+
+	gsap.to('.green',{
+		rotation: () => {
+
+			if(isMobile){
+				return 360
+			}else{
+				return -360
+			}
+		},
+		duration : 2,
+		ease : 'none',
+	})
+
+
+})
+// 3번째 인자는 해당 스코프 내에서만 작동
